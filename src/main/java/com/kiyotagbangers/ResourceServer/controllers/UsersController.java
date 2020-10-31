8b9d05690c7eb951d9a481028c8627512513d0a6
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-11-01 01:50:54 +0900

    preauthorize method

--- src/main/java/com/kiyotagbangers/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/kiyotagbangers/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,10 @@
 package com.kiyotagbangers.ResourceServer.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +16,12 @@ public class UsersController {
         return "working...";
     }
 
-    @Secured("ROLE_developer")
+    // @PreAuthorize("hasRole('developer')")
+    // @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+    @PreAuthorize("#id == #jwt.subject")
+    // @Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
-    public String deleteUser(@PathVariable String id){
-        return "Deleted user with id " + id;
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "Deleted user with id " + id + " and JWT subject" + jwt.getSubject();
     }
 }
