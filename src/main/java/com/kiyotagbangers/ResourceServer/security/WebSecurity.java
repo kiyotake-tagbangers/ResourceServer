24adb5cd2da5fb044dad460072b8ff599a999353
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-11-01 00:07:06 +0900

    add role base access control

--- src/main/java/com/kiyotagbangers/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/kiyotagbangers/ResourceServer/security/WebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -11,13 +12,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/status/check")
-                .hasAuthority("SCOPE_profile")
+                // scope base access control
+                // .hasAuthority("SCOPE_profile")
+
+                // role base access control
+                .hasRole("developer")
+                // if you specify multiple authority
+                // .hasAuthority("developer", "user")
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 }
